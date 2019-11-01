5c785603e02dda23e5e9643bbf44821ced0b4a91
Gabriel Flores <gabrielf@eventbrite.com>
2019-11-01 16:17:29 -0300

    initial print

--- vacations_app/views.py
+++ vacations_app/views.py
@@ -2,8 +2,16 @@
 from __future__ import unicode_literals
 
 from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
 
+from .models import Vacation
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name= 'vacations_app/index.html'
+
+
+# Create your views here.
+class VacationPrintView(DetailView):
+    template_name= 'vacations_app/vacation_print_form.html'
+    model = Vacation
