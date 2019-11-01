cf023149fa6ed629f583248b3ab6b39e1c19b1b0
Nahuel <nahuel.valencia@eventbrite.com>
2019-11-01 17:05:02 -0300

    settings

---
+++ vacations/settings/dev.py
@@ -0,0 +1,12 @@
+from .base import *  # noqa
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
+    }
+}
+
+DEBUG = True
---
+++ vacations/settings/production.py
@@ -0,0 +1,66 @@
+from .base import *  # noqa
+import dj_database_url
+
+ALLOWED_HOSTS = [
+    'vacationsqa.herokuapp.com',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vacations',
+        'USER': 'name',
+        'PASSWORD': '',
+        'PORT': '',
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_prod')
+
+INSTALLED_APPS += [  # noqa
+    'whitenoise.runserver_nostatic'
+]
+
+MIDDLEWARE += [  # noqa
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+]
+
+DB_FROM_ENV = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(DB_FROM_ENV)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] ' +
+                       'pathname=%(pathname)s lineno=%(lineno)s ' +
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'testlogger': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+DEBUG = False
