fd293d37ade5cec63b5c532892f07d15d7c7f4a8
Walter Bucolo <walter.bucolo@eventbrite.com>
2019-11-01 16:25:52 -0300

    list employee

--- vacations_app/views.py
+++ vacations_app/views.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from vacations_app.models import Employee
 
 
 # Create your views here.
-class HomeView(TemplateView):
+
+
+class HomeView(ListView):
     template_name= 'vacations_app/index.html'
+    model = Employee
