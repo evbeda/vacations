27fc742035af81051d276a5b050a279fd5564083
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-11-01 18:22:55 -0300

    Print to PDF

--- vacations_app/views.py
+++ vacations_app/views.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from io import BytesIO
 
-from django.views.generic import ListView
+from django.http import HttpResponse
+from django.template.loader import get_template
 from django.urls import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.base import TemplateView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 
 from vacations_app.models import Vacation
+from xhtml2pdf import pisa
+
+from .models import Vacation
 
 
 class HomeView(ListView):
@@ -23,11 +30,6 @@ class HomeView(ListView):
         return context
 
 
-class VacationPrintView(DetailView):
-    template_name= 'vacations_app/vacation_print_form.html'
-    model = Vacation
-
-
 class VacationRequest(CreateView):
     template_name='vacations_app/vacation_request.html'
     model = Vacation
@@ -39,3 +41,20 @@ class VacationRequest(CreateView):
     def form_valid(self, form):
         form.instance.employee = self.request.user
         return super().form_valid(form)
+
+
+class VacationPrintView(DetailView):
+    template_name = 'vacations_app/vacation_print_form.html'
+    model = Vacation
+
+    def render_to_response(self, context, **response_kwargs):
+        pdf = render_to_pdf(self.template_name, context)
+        return HttpResponse(pdf, content_type='application/pdf')        
+
+
+def render_to_pdf(template_src, context_dict={}):
+    template = get_template(template_src)
+    html = template.render(context_dict)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
+    return HttpResponse(result.getvalue(), content_type='application/pdf') if not pdf.err else None
