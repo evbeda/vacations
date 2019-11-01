fe4eec2f0b5d6c2852405bba57dc6ac594614b3e
Gabriel Flores <gabrielf@eventbrite.com>
2019-11-01 16:56:17 -0300

    calculated fields on print

--- vacations_app/models.py
+++ vacations_app/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from datetime import timedelta
 
 from django.db import models
+from django.db.models.aggregates import Sum
 from django.contrib.auth.models import User
 
 
@@ -21,3 +22,16 @@ class Vacation(models.Model):
     @property
     def to_date_next_day(self):
         return self.to_date + timedelta(days=1)
+
+    @property
+    def year_vacation_left(self):
+        vacations = Vacation.objects.filter(
+            employee=self.employee,
+            applicable_worked_year=self.applicable_worked_year,
+        )
+        total = 0
+        for vacation in vacations:
+            vacation_diff = vacation.to_date - vacation.from_date
+            total += vacation_diff.days
+        return self.employee.initial_annual_vacations_days - total
+
