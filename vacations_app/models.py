b72d9eade635470db950af69e1d36e49aa20d08a
Facundo Chaud <facundo.chaud@eventbrite.com>
2019-11-01 16:00:30 -0300

    Add models

--- vacations_app/models.py
+++ vacations_app/models.py
@@ -2,5 +2,15 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Employee(User):
+    job_start_date = models.DateField()
+    initial_annual_vacations_days = models.IntegerField(default=14)
+
+
+class Vacation(models.Model):
+    from_date = models.DateField()
+    to_date = models.DateField()
+    employee = models.ForeignKey('Employee')
