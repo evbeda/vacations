c17c92bef924b29cfa1f64da7794759f8ede271e
danielabertranou-eb <daniela.bertranou@eventbrite.com>
2019-11-05 17:20:27 -0300

    Add Holiday model, refactor validator for from_date field on Vacation model

--- vacations_app/admin.py
+++ vacations_app/admin.py
@@ -4,8 +4,10 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import (
     Employee,
+    Holiday,
     Vacation,
 )
 
 admin.site.register(Employee)
 admin.site.register(Vacation)
+admin.site.register(Holiday)
--- vacations_app/models.py
+++ vacations_app/models.py
@@ -3,17 +3,27 @@ from __future__ import unicode_literals
 
 from datetime import date, timedelta
 from django.db import models
-from django.core.mail import send_mail
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
-from django.utils.translation import ugettext_lazy as _
 from django.core.exceptions import ValidationError
 
-def validate_weekday(value):
-    if value.weekday() != 0:
-        raise ValidationError('The day you set to start your vacations must be a Monday')
-    else:
+
+def is_holiday(day):
+    return bool(Holiday.objects.filter(date=day))
+
+
+def validate_from_date(value):
+    if is_holiday(value):
+        raise ValidationError("You can't choose a holiday.")
+    if value.weekday() == 0:
         return value
+    previous_monday = value - timedelta(value.weekday())
+    for i in range((value - previous_monday).days):
+        if not is_holiday(previous_monday):
+            raise ValidationError('You must choose the first working day of the week.')
+        previous_monday += timedelta(days=1)
+    return value
+
 
 def validate_year(value):
     current_year = date.today().year
@@ -24,18 +34,14 @@ def validate_year(value):
 
 
 class UserManager(BaseUserManager):
-    use_in_migrations = True
 
     def _create_user(self, email, password, **extra_fields):
-        """
-        Creates and saves a User with the given email and password.
-        """
         if not email:
-            raise ValueError('The given email must be set')
+            raise ValueError('The Email must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
 
     def create_user(self, email, password=None, **extra_fields):
@@ -43,20 +49,24 @@ class UserManager(BaseUserManager):
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
 
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
-
-        return self._create_user(email, password, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
 
 
 class Employee(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(_('first name'), max_length=30, blank=True)
-    last_name = models.CharField(_('last name'), max_length=30, blank=True)
-    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
-    is_active = models.BooleanField(_('active'), default=True)
+    email = models.EmailField( unique=True)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     job_start_date = models.DateField(null=True)
     initial_annual_vacations_days = models.IntegerField(default=14)
 
@@ -65,28 +75,11 @@ class Employee(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-
     def get_full_name(self):
-        '''
-        Returns the first_name plus the last_name, with a space in between.
-        '''
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
+        return f'{self.first_name} {self.last_name}'
 
     def get_short_name(self):
-        '''
-        Returns the short name for the user.
-        '''
         return self.first_name
-
-    def email_user(self, subject, message, from_email=None, **kwargs):
-        '''
-        Sends an email to this User.
-        '''
-        send_mail(subject, message, from_email, [self.email], **kwargs)
 
 
 class Vacation(models.Model):
@@ -96,7 +89,7 @@ class Vacation(models.Model):
         (21, 21),
         (28, 28),
     ]
-    from_date = models.DateField(validators=[validate_weekday])
+    from_date = models.DateField(validators=[validate_from_date])
     days_quantity = models.IntegerField(choices=DAYS_CHOICES)
     to_date = models.DateField()
     employee = models.ForeignKey('Employee')
@@ -117,3 +110,11 @@ class Vacation(models.Model):
             vacation_diff = vacation.to_date - vacation.from_date
             total += vacation_diff.days
         return self.employee.initial_annual_vacations_days - total
+
+
+class Holiday(models.Model):
+    name = models.CharField(max_length=50)
+    date = models.DateField(unique=True)
+
+    def __str__(self):
+        return f'{self.date.year} - {self.name}'
