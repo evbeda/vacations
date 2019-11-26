c8b6f05b4c16636e918e83bb2877724c50e09b27
walter.bucolo <walter.bucolo@walter.bucolo-15530>
2019-11-26 10:38:35 -0300

    Employee filter was added

--- vacations_app/__init__.py
+++ vacations_app/__init__.py
@@ -1,2 +1,17 @@
 CAN_VIEW_OTHER_VACATIONS = 'vacations_app.can_view_other_vacations'
 CAN_VIEW_TEAM_MEMBERS_VACATIONS = 'vacations_app.can_view_team_members_vacations'
+MONTHS = [
+    (1, 'JANUARY'),
+    (2, 'FEBRUARY'),
+    (3, 'MARCH'),
+    (4, 'APRIL'),
+    (5, 'MAYO'),
+    (6, 'JUNE'),
+    (7, 'JULY'),
+    (8, 'MAYO'),
+    (8, 'AUGUST'),
+    (9, 'SEPTEMBER'),
+    (10, 'OCTOBER'),
+    (11, 'NOVEMBER'),
+    (12, 'DECEMBER'),
+]
