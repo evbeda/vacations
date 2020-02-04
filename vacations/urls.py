ace798a92045cac58b3224a4d8657cf64b7da6c7
Gabriel Flores <gabrielf@eventbrite.com>
2020-02-04 15:58:03 -0300

    add login page

--- vacations/urls.py
+++ vacations/urls.py
@@ -13,7 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import (
+    LogoutView,
+    LoginView,
+)
 from django.conf.urls import (
     include,
     url,
@@ -25,4 +28,6 @@ urlpatterns = [
     url(r'logout', LogoutView.as_view(), name='logout'),
     url(r'^', include('vacations_app.urls')),
     url('', include('social_django.urls', namespace='social')),
+    # the order is important... don't set login before social_django
+    url(r'login', LoginView.as_view(), name='login'),
 ]
